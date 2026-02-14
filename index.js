85adb15a6da96b806707c584db5f0338bae7f91c
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-02-14 23:00:25 +0530

    Fixed CORS for production

--- index.js
+++ index.js
@@ -25,7 +25,15 @@ connectCloudinary();
 // middleware;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-grocery-app.onrender.com"
+  ],
+  credentials: true
+}));
+
 app.use(cookieParser());
 
 //Api Endpoints;
