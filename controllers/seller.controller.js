bcd8e577d6e177be1b457d7ea6af333f6933c1c7
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-02-15 03:40:23 +0530

    Fix cookie issue

--- controllers/seller.controller.js
+++ controllers/seller.controller.js
@@ -24,8 +24,8 @@ export const sellerLogin = async (req, res) => {
 
         res.cookie("sellerToken", token, {
             httpOnly: true,
-            secure: true,
-            sameSite: "none",
+            secure: false,
+            sameSite: "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
