47fe4bebb8149458e11e0ada2c623f462c2bfb26
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-02-15 17:18:20 +0530

    fix cookie domain

--- controllers/seller.controller.js
+++ controllers/seller.controller.js
@@ -24,8 +24,10 @@ export const sellerLogin = async (req, res) => {
 
         res.cookie("sellerToken", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: true,
+            sameSite: "none",
+            domain: ".onrender.com",
+            path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
